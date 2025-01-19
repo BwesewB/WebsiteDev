4d275b51409af00c5a86998512cfc5bd0362035e
Sebastian <sebastianfok@gmail.com>
2025-01-18 22:58:05 -0800

    readding all files

--- .history/Three.js-lessons/03/exercise/script_20240520162704.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-//Scene
-const scene = new THREE.Scene()
-
-//Object
-const geometry = new THREE.BoxGeometry(1, 1, 1) 
-const material = new THREE.MeshBasicMaterial({ color: 'red' })
-
