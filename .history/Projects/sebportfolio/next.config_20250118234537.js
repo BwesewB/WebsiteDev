0990c21b191565bf684535f0df14b51a600f7c0d
Sebastian <sebastianfok@gmail.com>
2025-01-18 23:46:02 -0800

    Fix next.config.js for static export

---
+++ .history/Projects/sebportfolio/next.config_20250118234537.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    distDir: 'out', // Optional: Set the output directory for static files
+};
+
+module.exports = nextConfig;
