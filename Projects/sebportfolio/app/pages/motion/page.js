e8febcbf695976ceffb22699df6b3111ecd73436
Sebastian <sebastianfok@gmail.com>
2025-01-23 00:20:01 -0800

    added container at bottom of sticky video for video section

--- Projects/sebportfolio/app/pages/motion/page.js
+++ Projects/sebportfolio/app/pages/motion/page.js
@@ -8,8 +8,14 @@ export default function Motion() {
     const motionRef = useRef(null);
 
     return (
-        <div ref={motionRef} className={styles.motionContainer}>
-            <HeroSection videoSrc="/videos/Watermelon video.mp4" parentRef={motionRef} position="sticky"/>
+        <div className={styles.motionPage}>
+            <div ref={motionRef} className={styles.motionContainer}>
+                <HeroSection videoSrc="/videos/Watermelon video.mp4" parentRef={motionRef} position="sticky"/>
+            </div>
+            <div className={styles.motionContent}>
+
+            </div>
         </div>
+
     );
 }
