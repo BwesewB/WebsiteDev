6bef07917c0ed90ca77fc345e71ab45bd74f91f7
Sebastian <sebastianfok@gmail.com>
2025-01-29 21:57:35 -0800

    changed text for taxes

--- Projects/sebportfolio/app/pages/motion/projects/taxes/page.js
+++ Projects/sebportfolio/app/pages/motion/projects/taxes/page.js
@@ -17,7 +17,7 @@ export default function Taxes ({
                 />
                 <SectionFour 
                     sectionHeader="Taxation Frustration"
-                    sectionParagraph="Turning 18 introduced the unexpected challenge of taxes—an experience both frustrating and complex. To capture this, a lighthearted and comedic video was crafted to poke fun at the struggles of navigating taxes as a young adult. The creative direction shifted from using complex graphics to incorporating video clips, with graphic elements setting up what would unfold in the video. This decision allowed for smoother storytelling and dynamic pacing, providing a fresh and engaging way to turn a stressful situation into something relatable and entertaining."
+                    sectionParagraph="Turning 18 introduced the unexpected challenge of taxes—an experience both frustrating and complex. To capture this, a lighthearted and comedic video was crafted to poke fun at the struggles of navigating taxes as a young adult with a light and colorful theme. The creative direction shifted from using complex graphics to incorporating video clips, with graphic elements setting up what would unfold in the video. This decision allowed for smoother storytelling and dynamic pacing, providing a fresh and engaging way to turn a stressful situation into something relatable and entertaining."
                     endTrigger="bottom bottom"
                 />
             </div>
