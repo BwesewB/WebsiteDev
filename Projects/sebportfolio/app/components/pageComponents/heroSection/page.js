d9afbb477c65761c6a72e6e6c93b9e9f27014393
BwesewB <sebastianfok@gmail.com>
2025-02-25 14:44:17 -0800

    optimized gsap animation for heroSection

--- Projects/sebportfolio/app/components/pageComponents/heroSection/page.js
+++ Projects/sebportfolio/app/components/pageComponents/heroSection/page.js
@@ -2,7 +2,7 @@
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./heroSection.module.css"
 
 gsap.registerPlugin(ScrollTrigger);
@@ -13,57 +13,56 @@ export default function HeroSection({
     position = "fixed"
 }) {
 
-    useEffect(() => {
-      const ctx = gsap.context(() => {
-        gsap.fromTo(`.${styles.letter}`, {
-          y:40,
-        },
+  const titleRef = useRef(null);
+  const japaneseRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      // Animate title letters
+      gsap.fromTo(
+        titleRef.current?.children,
+        { y: 40, opacity: 0 },
         {
           y: 0,
           opacity: 1,
           duration: 0.6,
-          ease: "expoScale",
-          stagger: 0.05, // Stagger for smoother animation
-        });
-      });
+          ease: "expo.out",
+          stagger: 0.05,
+        }
+      );
 
-      return () => ctx.revert(); // Cleanup GSAP animation on unmount
-    }, []);
-
-    useEffect(() => {
-      const ctx = gsap.context(() => {
-        gsap.fromTo(`.${styles.japanese}`, {
-          x:-40,
-          rotate:-10,
-        },
+      // Animate Japanese text
+      gsap.fromTo(
+        japaneseRef.current?.children,
+        { x: -40, rotate: -10, opacity: 0 },
         {
           x: 0,
-          rotate:0,
-          delay:0.5,
+          rotate: 0,
           opacity: 1,
           duration: 0.6,
-          ease: "expoScale",
-          stagger: 0.05, // Stagger for smoother animation
-        });
-      });
+          ease: "expo.out",
+          delay: 0.3,
+          stagger: 0.05,
+        }
+      );
+    });
 
-      return () => ctx.revert(); // Cleanup GSAP animation on unmount
-    }, []);
+    return () => ctx.revert(); // Cleanup GSAP animation on unmount
+  }, []);
 
     return (
       <div className={styles.hero} style={{position:position}}>
-          <div className={styles.title}>
+          <div className={styles.title} ref={titleRef}>
             {heroSectionTitle.split("").map((char, index) => (
               <h1 key={index} className={styles.letter}>{char === " " ? "\u00A0" : char}</h1>
             ))}
           </div>
 
-          <div className={styles.japaneseWrap}>
+          <div className={styles.japaneseWrap} ref={japaneseRef}>
             {japaneseText.split("").map((char, index) => (
               <h3 key={index} className={styles.japanese}>{char === " " ? "\u00A0" : char}</h3>
             ))}
           </div>
-
       </div>
     )
 }
