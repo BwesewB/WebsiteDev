b7bd7550f592bea67de873873cacc0943a0fba81
Sebastian <sebastianfok@gmail.com>
2025-03-01 00:27:59 -0800

    added muted to video so that autoplay works

--- Projects/sebportfolio/app/components/pageComponents/preloader/page.js
+++ Projects/sebportfolio/app/components/pageComponents/preloader/page.js
@@ -30,6 +30,7 @@ export default function Preloader() {
                     autoPlay
                     preload="auto"
                     playsInline
+                    muted
                 />
             </div>
         </>
