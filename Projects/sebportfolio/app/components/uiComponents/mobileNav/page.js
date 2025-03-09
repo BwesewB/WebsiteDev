a1768afd4e0abb74bdfa66db72f32c2c9219c751
Sebastian <sebastianfok@gmail.com>
2025-03-09 00:22:12 -0800

    removed commented not working code

--- Projects/sebportfolio/app/components/uiComponents/mobileNav/page.js
+++ Projects/sebportfolio/app/components/uiComponents/mobileNav/page.js
@@ -13,77 +13,19 @@ CustomEase.create("rollingEase", "M0,0 C0.2,0.8 0.4,1.2 1,1");
 export default function MobileNavbar({ navColor, textColor }) {
   const [isActive, setIsActive] = useState(false);
 
-  const arrowMobileRef = useRef(null);
   const arrowMobileCircleRef = useRef(null);
   const mobileMenuRef = useRef(null);
 
   const mobileNavBackgroundWrapRef = useRef(null);
 
-//   const handleArrowClick = () => {
-//     const mobileMenuCircle = arrowMobileCircleRef.current;
-//     const mobileMenu = mobileMenuRef.current;
-
-//     setIsActive((prevState) => !prevState);
-
-//     const tl = gsap.timeline();
-
-//     if (isActive) {
-//       gsap.to(mobileMenuCircle, {
-//         borderRadius: "100%", 
-//         duration: 0.6,
-//         ease: "power1.inOut",
-//       });
-
-//       tl.to(mobileMenu, {
-//         width: "auto",
-//         height: "60px",
-//         borderRadius: "100%",
-//         transformOrigin: "top right",
-//         duration: 0.6,
-//         ease: "power1.inOut",
-//       })
-//       .to(
-//         mobileMenu,
-//         {
-//             margin: "var(--sideSpacing)",
-//             padding: 0,
-//             duration: 0.6,
-//         },
-//         "-=1"
-//       )
-      
-//     } else {
-//       gsap.to(mobileMenuCircle, {
-//         borderRadius: "0%",
-//         duration: 0.6,
-//         ease: "power1.inOut",
-//       });
-
-//       tl.to(mobileMenu, {
-//         top: 0,
-//         right: 0,
-
-//         height: "100vh",
-//         width: "100vw",
-//         borderRadius: "0%",
-//         transformOrigin: "top right",
-//         duration: 0.6,
-//         ease: "power1.inOut",
-//       }, )
-//       .to(
-//         mobileMenu,
-//         {
-//             margin: 0,
-//             padding: "var(--sideSpacing)",
-//             duration: 0.6,
-//         },
-//         "-=1"
-//       )
-//     }
-//   };
-
 const handleArrowClick = () => {
-    setIsActive((prevState) => !prevState);
+    setIsActive(prevState => !prevState);
+
+    if (!isActive) {
+        document.body.style.overflow = "hidden"; // Disable scrolling
+      } else {
+        document.body.style.overflow = "auto"; // Enable scrolling again
+      }
 
     if (isActive) {
         gsap.to(arrowMobileCircleRef.current, {
@@ -101,6 +43,7 @@ const handleArrowClick = () => {
             borderTopLeftRadius: "100%",
             transformOrigin: "top right",
             duration: 0.6,
+            ease: "power1.inOut",
         });
     } else {
         gsap.to(arrowMobileCircleRef.current, {
@@ -118,37 +61,38 @@ const handleArrowClick = () => {
             borderTopLeftRadius: "0%",
             transformOrigin: "top right",
             duration: 1,
+            ease: "power1.inOut",
         });
     }
   };
 
   useEffect(() => {
-    gsap.to(mobileMenuRef.current, {
-      margin: "var(--sideSpacing)",
-      x:0,
-      duration: 0.9,
-      scale: 1,
-      ease: "rollingEase",
-    });
+        gsap.to(mobileMenuRef.current, {
+            margin: "var(--sideSpacing)",
+            x: 0,
+            duration: 0.9,
+            scale: 1,
+            ease: "rollingEase",
+        });
 
-    gsap.to(arrowMobileCircleRef.current, {
-      scale: 1,
-      duration: 0.6,
-      ease: "power4.out",
-    });
+        gsap.to(arrowMobileCircleRef.current, {
+            scale: 1,
+            duration: 0.6,
+            ease: "power4.out",
+        });
   }, []);
 
   return (
     <div className={styles.mobileNav} ref={mobileMenuRef} style={{ backgroundColor: navColor }}>
-      <div className={styles.arrowDivMobile} ref={arrowMobileCircleRef} onClick={handleArrowClick}>
-        <div className={styles.arrowMobileContainer} ref={arrowMobileRef} style={{ transform: "rotate(180deg)" }}>
-          <ArrowRight color={textColor} width="auto" height="auto" />
+        <div className={styles.arrowDivMobile} ref={arrowMobileCircleRef} onClick={handleArrowClick}>
+            <div className={styles.arrowMobileContainer}>
+                <ArrowRight color={textColor} width="auto" height="auto" />
+            </div>
         </div>
-      </div>
 
-      <div className={styles.mobileNavBackgroundWrap} ref={mobileNavBackgroundWrapRef} style={{ backgroundColor: navColor }}>
+        <div className={styles.mobileNavBackgroundWrap} ref={mobileNavBackgroundWrapRef} style={{ backgroundColor: navColor }}>
 
-      </div>
+        </div>
     </div>
   );
 }
