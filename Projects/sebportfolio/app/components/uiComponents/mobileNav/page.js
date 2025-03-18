5c784f3987ff4806255e720d115dc023df25c105
Sebastian <sebastianfok@gmail.com>
2025-03-18 16:16:24 -0700

    adjusted navbard margins and animations

--- Projects/sebportfolio/app/components/uiComponents/mobileNav/page.js
+++ Projects/sebportfolio/app/components/uiComponents/mobileNav/page.js
@@ -67,10 +67,7 @@ const MobileNavbar = ({ navColor, textColor }) => {
   useEffect(() => {
     if (typeof window !== "undefined") {
       gsap.to(mobileMenuRef.current, {
-        marginTop: "var(--sideSpacing)",
-        marginLeft: "var(--sideSpacing)",
-        marginBottom: "var(--sideSpacing)",
-        marginRight: "calc( 2 * var(--sideSpacing))",
+        // margin: "calc( 2 * var(--sideSpacing))",
         x: 0,
         duration: 0.9,
         scale: 1,
