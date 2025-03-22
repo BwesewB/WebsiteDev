524bc65064d1b8db961b38bab514ddff5acc755a
Sebastian <sebastianfok@gmail.com>
2025-03-22 15:20:46 -0700

    dude i just disabled hydration warning in my laout and it dissapeared... so cool

--- Projects/sebportfolio/app/layout.js
+++ Projects/sebportfolio/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ padding:"0", margin:"0" }}>
-      <body>
+      <body suppressHydrationWarning={true}>
         <ClientWrap>{children}</ClientWrap>
       </body>
     </html>
