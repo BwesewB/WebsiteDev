ad9434481d9a33bb95ac5abae59f79be1acb60fc
Sebastian <sebastianfok@gmail.com>
2025-07-09 22:32:39 -0700

    added arrow hover effect on projectCard

--- Projects/portfoliotwo/components/templates/projectCard/projectCard.js
+++ Projects/portfoliotwo/components/templates/projectCard/projectCard.js
@@ -1,8 +1,11 @@
-
+import { useRef } from 'react';
 import MediaBlock from '@/components/molecules/MediaBlock/MediaBlock';
 import DynamicHover from '@/components/molecules/DynamicHover/DynamicHover';
 import TextContainer from '@/components/atoms/textContainer/page';
+import ArrowRight from '@/components/atoms/arrowRight';
 import styles from './projectCard.module.css';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 export default function ProjectCard({
     link,
@@ -15,8 +18,58 @@ export default function ProjectCard({
     movementFactor,
 }) {
 
+    const arrowRef = useRef(null);
+    const arrowContainerRef = useRef(null); 
+    const projectCardRef = useRef(null);
+    const textContainerRef = useRef(null);
+
+    useGSAP(() => {
+
+        const textElement = textContainerRef.current;
+        const arrowContainerElement = arrowContainerRef.current;
+        const arrowElement = arrowRef.current;
+
+        const tl = gsap.timeline({ paused: true });
+
+        tl.to(textElement, {
+            x: 50,
+            duration: 0.3,
+            ease: "power3.out",
+        }, 0); // Start at 0 seconds
+
+        tl.to(arrowContainerElement, {
+            x: 0,
+            duration: 0.3,
+            ease: "power3.out",
+            scale: 1,
+        }, 0.1); // Start at 0.1 seconds (creates the delay)
+
+        tl.to(arrowElement, {
+            scale:1,
+            duration: 0.25,
+            ease: "power3.inOut",
+        }, 0.1)
+
+        const handleMouseEnter = () => {
+            tl.play(); 
+        };
+
+        const handleMouseLeave = () => {
+            tl.reverse(); // Reverse the timeline back to its start
+        };
+
+        const projectCard = projectCardRef.current;
+        projectCard.addEventListener("mouseenter", handleMouseEnter);
+        projectCard.addEventListener("mouseleave", handleMouseLeave);
+
+        return () => {
+            projectCard.removeEventListener("mouseenter", handleMouseEnter);
+            projectCard.removeEventListener("mouseleave", handleMouseLeave);
+        };
+    }, { scope: projectCardRef });
+
   return (
-    <div className={styles.projectCardWrap}>
+    <div className={styles.projectCardWrap} ref={projectCardRef}>
         <DynamicHover
             scale={scale}
             movementFactor={movementFactor}
@@ -33,10 +86,21 @@ export default function ProjectCard({
             )}
         </DynamicHover>
         {paragraph && <p className={styles.paragraph}>{paragraph}</p>}
-        <TextContainer
-            header={header}
-            width="100%"
-        />
+        <div className={styles.textContainer}>
+            <div className={styles.arrowDiv} ref={arrowContainerRef}>
+                <div className={styles.arrowContainer} ref={arrowRef}>
+                    <ArrowRight color="var(--black)" width="100%" height="100%"/>
+                </div>
+            </div>
+            <div ref={textContainerRef}>
+                <TextContainer
+                    header={header}
+                    width="100%"
+                />
+            </div>
+
+        </div>
+
     </div>
   );
 }
